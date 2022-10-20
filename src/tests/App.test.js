d07d7b455be565e6c93cacea5a0a117b705efc1b
Joao Victor <imashinzo10@gmail.com>
2022-10-20 15:01:36 -0300

    Requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,40 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('1. Teste o componente <App.js />', () => {
+  test('Verifica se existe um link Home e seu redirecionamento', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const homeLink = screen.getByRole('link', { name: 'Home' });
+    expect(homeLink).toBeInTheDocument();
+
+    userEvent.click(homeLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+
+  test('Verifica se existe um link About e seu redirecionamento', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const aboutLink = screen.getByRole('link', { name: 'About' });
+    expect(aboutLink).toBeInTheDocument();
+
+    userEvent.click(aboutLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  });
+
+  test('Verifica se existe Favorite Pokémons e seu redirecionamento', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const favoritesLink = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    expect(favoritesLink).toBeInTheDocument();
+
+    userEvent.click(favoritesLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  });
+});
